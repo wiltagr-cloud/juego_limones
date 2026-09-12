1f8a6d2a3465731cbf57b981f915bc8a0c81c39a
wiltagr-cloud <wiltagr@icloud.com>
2026-09-11 21:48:54 -0500

    mover ala izquierda

--- limones.js
+++ limones.js
@@ -4,6 +4,7 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+let peronajeX=canvas.width/2;
 
 function iniciar(){
     dibujarSuelo();
@@ -18,5 +19,22 @@ function dibujarSuelo() {
 
 function dibujarPersonaje(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
+    ctx.fillRect(peronajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
+}
+
+function moverIzquierda(){
+    peronajeX=peronajeX-10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
 }
