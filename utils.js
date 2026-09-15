f4c256a396101eac5b9cc732577d8efd33f51249
wiltagr-cloud <wiltagr@icloud.com>
2026-09-15 16:34:39 -0500

    corregir errore

--- utils.js
+++ utils.js
@@ -2,12 +2,10 @@ function generarAleatorio(min, max){
 
     let random = Math.random();
 
-    // Ejemplo: max es 600, mínimo es 5
     let numero = random * (max - min);
 
     let numeroEntero = Math.ceil(numero);
 
-    // Sumamos el mínimo
     numeroEntero = numeroEntero + min;
 
     return numeroEntero;
